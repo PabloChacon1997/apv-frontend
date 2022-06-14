dcfa62ab52a18737d2ab1ec89241b7a3f34fb707
PabloChacon1997 <pabloandres120297@gmail.com>
2022-06-14 15:39:49 -0500

    Creando los formularios y estilos tailwindcss

--- src/paginas/Login.jsx
+++ src/paginas/Login.jsx
@@ -1,8 +1,49 @@
+import { Link } from 'react-router-dom'
+
 
 const Login = () => {
   return (
     <>
-      <h2>Login</h2>
+      <div>
+        <h1 className="text-indigo-600 font-black text-6xl">
+          Inicia Sesion y Administra tus <span className="text-black">Pacientes</span>
+        </h1>
+      </div>
+      <div className='mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl font-bold'>
+        <form action="">
+          <div className="my-5">
+            <label 
+              className="uppercase text-gray-600 block text-xl font-bold" 
+              htmlFor="email">Email</label>
+            <input
+              name="email"
+              id="email"
+              type="text" 
+              placeholder="Email de registro" 
+              className="border w-full p-3 mt-3 bg-gray-50 rounded-xl" />
+          </div>
+          <div className="my-5">
+            <label 
+              className="uppercase text-gray-600 block text-xl font-bold" 
+              htmlFor="password">Password</label>
+            <input
+              name="password"
+              id="password"
+              type="password" 
+              placeholder="Tu password" 
+              className="border w-full p-3 mt-3 bg-gray-50 rounded-xl" />
+          </div>
+
+          <input 
+            type="submit" 
+            value="Iniciar Sesión"
+            className="bg-indigo-700 w-full py-3 px-10 rounded-xl text-white uppercase font-bold mt-5 hover:cursor-pointer hover:bg-indigo-800 md:w-auto" />
+        </form>
+        <nav className='mt-10 lg:flex lg:justify-between'>
+          <Link className='block text-center my-5 text-gray-500' to="/registrar">¿No tienes cuenta? Registrate</Link>
+          <Link className='block text-center my-5 text-gray-500' to="/olvide-password">Olvide mi password</Link>
+        </nav>
+      </div>
     </>
   )
 }
--- src/paginas/Registrar.jsx
+++ src/paginas/Registrar.jsx
@@ -1,7 +1,119 @@
+import { useState } from "react"
+import { Link } from "react-router-dom"
+import Alerta from "../components/Alerta"
+
+
 
 const Registrar = () => {
+
+  const [nombre, setNombre] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [repetirpassword, setRepetirPassword] = useState('');
+  const [alerta, setAlerta] = useState({});
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if([nombre, email, password, repetirpassword].includes('')) {
+      setAlerta({msg :'Hay campos vacios', error: true});
+      return;
+    }
+    if (password !== repetirpassword) {
+      setAlerta({msg: 'Los passowords no son iguales', error: true});
+      return;
+    }
+
+    if (password.length < 6) {
+      setAlerta({msg: 'El password es muy corto, agrega minimo 6 caracteres', error: true});
+      return;
+    }
+
+    setAlerta({});
+
+    // Crear el usuario en la API
+  }
+
+  const {msg} = alerta;
+
   return (
-    <h1>Registrar</h1>
+    <>
+      <div>
+        <h1 className="text-indigo-600 font-black text-6xl">
+          Crea tu Cuenta y Administra <span className="text-black">tus Pacientes</span>
+        </h1>
+      </div>
+      <div className='mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl font-bold'>
+        {
+          msg && <Alerta 
+          alerta={alerta}
+        />
+        }
+        <form 
+          onSubmit={handleSubmit}
+        >
+          <div className="my-5">
+            <label 
+              className="uppercase text-gray-600 block text-xl font-bold" 
+              htmlFor="emailnombrEmail">Nombre</label>
+            <input
+              name="nombre"
+              id="nombre"
+              type="text" 
+              placeholder="Tu nombre" 
+              className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+              value={nombre}
+              onChange={e => setNombre(e.target.value)} />
+          </div>
+          <div className="my-5">
+            <label 
+              className="uppercase text-gray-600 block text-xl font-bold" 
+              htmlFor="email">Email</label>
+            <input
+              name="email"
+              id="email"
+              type="email" 
+              placeholder="Tu email" 
+              className="border w-full p-3 mt-3 bg-gray-50 rounded-xl" 
+              value={email}
+              onChange={e => setEmail(e.target.value)} />
+          </div>
+          <div className="my-5">
+            <label 
+              className="uppercase text-gray-600 block text-xl font-bold" 
+              htmlFor="password">Password</label>
+            <input
+              name="password"
+              id="password"
+              type="password" 
+              placeholder="Tu password" 
+              className="border w-full p-3 mt-3 bg-gray-50 rounded-xl" 
+              value={password}
+              onChange={e => setPassword(e.target.value)} />
+          </div>
+          <div className="my-5">
+            <label 
+              className="uppercase text-gray-600 block text-xl font-bold" 
+              htmlFor="repitepassword">Repetir Password</label>
+            <input
+              name="repitepassword"
+              id="repitepassword"
+              type="password" 
+              placeholder="Repite tu password" 
+              className="border w-full p-3 mt-3 bg-gray-50 rounded-xl" 
+              value={repetirpassword}
+              onChange={e => setRepetirPassword(e.target.value)} />
+          </div>
+          <input 
+            type="submit" 
+            value="Crear cuenta"
+            className="bg-indigo-700 w-full py-3 px-10 rounded-xl text-white uppercase font-bold mt-5 hover:cursor-pointer hover:bg-indigo-800 md:w-auto" />
+          <nav className='mt-10 lg:flex lg:justify-between'>
+            <Link className='block text-center my-5 text-gray-500' to="/">¿Ya tienes cuenta? Inicia sesion</Link>
+            <Link className='block text-center my-5 text-gray-500' to="/olvide-password">Olvide mi password</Link>
+          </nav>
+        </form>
+      </div> 
+    </>
   )
 }
 

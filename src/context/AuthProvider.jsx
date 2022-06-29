a64c2b51a68072b0d00187cb6676ff6367f1913f
PabloChacon1997 <pabloandres120297@gmail.com>
2022-06-29 15:55:47 -0500

    Autenticacion veterinario

---
+++ src/context/AuthProvider.jsx
@@ -0,0 +1,55 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import { createContext } from "react";
+import clienteAxios from '../config/axios'
+
+
+const AuthContext = createContext()
+
+
+const AuthProvider = ({children}) => {
+
+  const [auth, setAuth] = useState({})
+
+  useEffect(() => {
+    const autenticarUsuario = async () => {
+      const token = localStorage.getItem('token')
+      if(!token) return;
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`
+        }
+      }
+
+      try {
+        const { data } = await clienteAxios('/veterinarios/perfil',config)
+        console.log(data);
+      } catch (error) {
+        console.log(error.response.data.msg);
+        setAuth({})
+      }
+    }
+    autenticarUsuario()
+  }, [])
+  
+
+  return(
+    <AuthContext.Provider
+      value={{
+        auth,
+        setAuth
+      }}
+    >
+      { children }
+    </AuthContext.Provider>
+  )
+}
+
+
+export {
+  AuthProvider
+}
+
+
+export default AuthContext
--- src/paginas/Login.jsx
+++ src/paginas/Login.jsx
@@ -1,7 +1,45 @@
-import { Link } from 'react-router-dom'
+import { useState} from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import Alerta from '../components/Alerta'
+import clienteAxios from '../config/axios'
+import useAuth from '../hooks/useAuth'
 
 
 const Login = () => {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [alerta, setAlerta] = useState({})
+
+  const navigate = useNavigate()
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if([ email, password].includes('')) {
+      setAlerta({msg :'Hay campos vacios', error: true});
+      return;
+    }
+
+    try {
+      const { data } = await clienteAxios.post('/veterinarios/login', { email, password })
+      localStorage.setItem('token', data.token)
+
+      navigate('/admin')
+    } catch (error) {
+      setAlerta({
+        msg: error.response.data.msg,
+        error: true
+      });
+    }
+  }
+
+  //  const { auth } = useAuth()
+
+  //  console.log(auth);
+
+   const {msg} = alerta;
+
   return (
     <>
       <div>
@@ -10,7 +48,12 @@ const Login = () => {
         </h1>
       </div>
       <div className='mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl font-bold'>
-        <form action="">
+      {
+          msg && <Alerta 
+          alerta={alerta}
+        />
+        }
+        <form onSubmit={handleSubmit}>
           <div className="my-5">
             <label 
               className="uppercase text-gray-600 block text-xl font-bold" 
@@ -20,7 +63,9 @@ const Login = () => {
               id="email"
               type="text" 
               placeholder="Email de registro" 
-              className="border w-full p-3 mt-3 bg-gray-50 rounded-xl" />
+              className="border w-full p-3 mt-3 bg-gray-50 rounded-xl" 
+              value={email}
+              onChange={e => setEmail(e.target.value)}/>
           </div>
           <div className="my-5">
             <label 
@@ -31,7 +76,9 @@ const Login = () => {
               id="password"
               type="password" 
               placeholder="Tu password" 
-              className="border w-full p-3 mt-3 bg-gray-50 rounded-xl" />
+              className="border w-full p-3 mt-3 bg-gray-50 rounded-xl" 
+              value={password}
+              onChange={e => setPassword(e.target.value)}/>
           </div>
 
           <input 
